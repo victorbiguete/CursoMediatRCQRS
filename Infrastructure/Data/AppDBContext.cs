9d0f54196bfa820dda58943f24b004ce3b39622f
victorbiguete <victorbiguete@gmail.com>
2025-05-16 14:38:41 -0300

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMediatRCQRS.Infrastructure.Data
+{
+    public class AppDBContext : DbContext
+    {
+        protected AppDBContext()
+        {
+
+        }
+    }
+}
